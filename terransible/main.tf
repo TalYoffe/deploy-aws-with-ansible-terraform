1f802b0b0877be640b2a28ebf6d808e348330d01
root <root@ip-172-31-33-56.ec2.internal>
2018-04-06 13:51:23 +0000

    added subnets

--- terransible/main.tf
+++ terransible/main.tf
@@ -99,3 +99,49 @@ resource "aws_default_route_table" "wp_private_rt" {
     Name = "wp_private"
   }
 }
+
+#Subnets
+
+resource "aws_subnet" "wp_public1_subet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zone.available.names[0]}"
+
+  tags {
+    Name = "wp_public1"
+  }
+}
+
+resource "aws_subnet" "wp_public1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_public2"
+  }
+}
+
+resource "aws_subnet" "wp_private1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_private1"
+  }
+}
+
+resource "aws_subnet" "wp_private2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_private2"
+  }
+}
