9d4761f01f6bd1ebd2d54a475e0e4dc8a8dd2fca
root <root@ip-172-31-33-56.ec2.internal>
2018-04-11 17:23:19 +0000

    added ASG

--- terransible/main.tf
+++ terransible/main.tf
@@ -438,3 +438,74 @@ resource "aws_elb" "wp_elb" {
     Name = "wp_${var.domain_name}-elb"
   }
 }
+
+#-------Ami---------------------
+
+resource "random_id" "golden_ami" {
+  byte_length = 8
+}
+
+resource "aws_ami_from_instance" "wp_golden" {
+  name               = "wp_ami-${random_id.golden_ami.b64}"
+  source_instance_id = "${aws_instance.wp_dev.id}"
+
+  provisioner "local-exec" {
+    command = <<EOT
+cat <<EOF > userdata
+#!/bin/bash
+/usr/bin/aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/
+/bin/touch /var/spool/cron/root
+sudo /bin/echo '*/5 * * * * aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/' >> /var/spool/cron/root
+EOF
+EOT
+  }
+}
+
+#launch configuration
+
+resource "aws_launch_configuration" "wp_lc" {
+  name_prefix          = "wp_lc-"
+  image_id             = "${aws_ami_from_instance.wp_golden.id}"
+  instance_type        = "${var.lc_instance_type}"
+  security_groups      = ["${aws_security_group.wp_private_sg.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.s3_access_profile.id}"
+  key_name             = "${aws_key_pair.wp_auth.id}"
+  user_data            = "${file("userdata")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#ASG 
+
+#resource "random_id" "rand_asg" {
+# byte_length = 8
+#}
+
+resource "aws_autoscaling_group" "wp_asg" {
+  name                      = "asg-${aws_launch_configuration.wp_lc.id}"
+  max_size                  = "${var.asg_max}"
+  min_size                  = "${var.asg_min}"
+  health_check_grace_period = "${var.asg_grace}"
+  health_check_type         = "${var.asg_hct}"
+  desired_capacity          = "${var.asg_cap}"
+  force_delete              = true
+  load_balancers            = ["${aws_elb.wp_elb.id}"]
+
+  vpc_zone_identifier = ["${aws_subnet.wp_private1_subnet.id}",
+    "${aws_subnet.wp_private2_subnet.id}",
+  ]
+
+  launch_configuration = "${aws_launch_configuration.wp_lc.name}"
+
+  tag {
+    key                 = "Name"
+    value               = "wp_asg-instance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
